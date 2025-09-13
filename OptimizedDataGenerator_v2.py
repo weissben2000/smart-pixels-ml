17967ee677b5e67e0a0de26f9a35f59d78cc0624
Ben Weiss <bw539@cornell.edu>
2025-09-13 16:55:58 +0000

    max and min threshold options added

--- OptimizedDataGenerator_v2.py
+++ OptimizedDataGenerator_v2.py
@@ -55,9 +55,10 @@ class OptimizedDataGenerator(tf.keras.utils.Sequence):
             tfrecords_dir: str = None,
             use_time_stamps = -1,
             select_contained = False, #If true, selects only clusters with original_atEdge==False
-            noise = -1, #added gaussian noise (mu, sigma), set to -1 to turn off
+            noise = -1, #add gaussian noise (mu, sigma), set to -1 to turn off
             seed: int = None,
-            threshold = -1,
+            min_threshold: float = None, #Zeros out charge<min_thresh
+            max_threshold: float = None, #Zeros out charge>max_thresh
             quantize: bool = False,
             max_workers: int = 1,
             label_scale_pctl: float = 99,
@@ -115,8 +116,10 @@ class OptimizedDataGenerator(tf.keras.utils.Sequence):
             self.to_standardize = to_standardize
             self.noise = noise
             self.select_contained = select_contained
-            self.threshold = threshold
-
+            self.min_threshold = min_threshold
+            self.max_threshold = max_threshold
+            if (max_threshold is not None) and (min_threshold is not None) and (max_threshold < min_threshold):
+                raise ValueError("max_threshold < min_threshold!")
 
             self.process_file_parallel()
             
@@ -188,7 +191,8 @@ class OptimizedDataGenerator(tf.keras.utils.Sequence):
             "shuffle": self.shuffle,
             "noise": self.noise,
             "select_contained": self.select_contained,
-            "threshold": self.threshold,
+            "min_threshold": self.min_threshold,
+            "max_threshold": self.max_threshold,
             
             "seed": self.seed,
             "label_scale_pctl": self.label_scale_pctl,
@@ -257,14 +261,16 @@ class OptimizedDataGenerator(tf.keras.utils.Sequence):
         self.seed = metadata.get('seed', 13)
         self.transpose = metadata.get('transpose', None)
         self.noise = metadata.get('noise', -1)
-        self.threshold = metadata.get('threshold', -1)
+        self.min_threshold = metadata.get('min_threshold', None)
+        self.max_threshold = metadata.get('max_threshold', None)
+
         if self.shuffle:
             self.rng = np.random.default_rng(seed=self.seed)
             
 
     def process_file_parallel(self):
         file_infos = [(afile, 
-                    self.recon_cols, self.labels_list, self.noise, self.threshold, self.select_contained, 
+                    self.recon_cols, self.labels_list, self.noise, self.min_threshold, self.max_threshold, self.select_contained, 
                     self.label_scale_pctl, self.norm_pos_pctl, self.norm_neg_pctl) 
                     for afile in self.files
                     ]
@@ -305,7 +311,7 @@ class OptimizedDataGenerator(tf.keras.utils.Sequence):
 
     @staticmethod
     def _process_file_single(file_info):
-        afile, recon_cols, labels_list, noise, threshold, select_contained, label_scale_pctl, norm_pos_pctl, norm_neg_pctl = file_info
+        afile, recon_cols, labels_list, noise, min_threshold, max_threshold, select_contained, label_scale_pctl, norm_pos_pctl, norm_neg_pctl = file_info
         if select_contained:
             df = (pd.read_parquet(afile, 
                                  columns=recon_cols + labels_list +['original_atEdge'])
@@ -321,9 +327,12 @@ class OptimizedDataGenerator(tf.keras.utils.Sequence):
         if noise != -1:
             bkg = np.random.normal(*noise, x.shape)
             x = x+bkg
-        if threshold != -1:
-            bellowthresh = x < threshold
+        if min_threshold is not None:
+            bellowthresh = x < min_threshold
             x[bellowthresh] = 0*x[bellowthresh]
+        if max_threshold is not None:
+            abovethresh = x > max_threshold
+            x[abovethresh] = 0*x[abovethresh]
             
         
         nonzeros = abs(x) > 0
@@ -540,9 +549,13 @@ class OptimizedDataGenerator(tf.keras.utils.Sequence):
                 if self.noise !=-1:
                     bkg = np.random.normal(*self.noise, recon_values.shape)
                     recon_values = recon_values + bkg
-                if self.threshold != -1: 
-                    bellowthresh = recon_values < self.threshold
+                if self.min_threshold is not None: 
+                    bellowthresh = recon_values < self.min_threshold
                     recon_values[bellowthresh] = 0*recon_values[bellowthresh]
+                if self.max_threshold is not None: 
+                    abovethresh = recon_values > self.max_threshold
+                    recon_values[abovethresh] = 0*recon_values[abovethresh]
+                
                 nonzeros = abs(recon_values) > 0
                 recon_values[nonzeros] = np.sign(recon_values[nonzeros]) * np.log1p(abs(recon_values[nonzeros])) / np.log(2)
                 if self.to_standardize:
